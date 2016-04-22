aaf0da1c4d3d7385bb2c78bfb894e2be1835383a
Ramatoulie Sanyang <misramz@yahoo.com>
2016-04-22 16:41:13 -0400

    url applyied and css adjustments where made

--- app/js/bundle.js
+++ app/js/bundle.js
@@ -9851,6 +9851,7 @@ var _jquery = require('jquery');
 
 var _jquery2 = _interopRequireDefault(_jquery);
 
+//console.log($);
 //
 // $.ajax({
 //   url: 'https://randomuser.me/api/',
@@ -9860,11 +9861,33 @@ var _jquery2 = _interopRequireDefault(_jquery);
 //   }
 // });
 
-var baseURL = 'https://randomuser.me/api/?results=12&nat=us';
-var dataReq = _jquery2['default'].getJSON(url);
+//var url= 'https://randomuser.me/api/';
+// var dataReq = $.getJSON(url).then(function(res){
+//   console.log('res',res);
+//});
 
 var inner = (0, _jquery2['default'])('.inner');
-console.log(url);
+var baseURL = 'https://randomuser.me/api/?results=12&nat=us';
+var dataReq = _jquery2['default'].getJSON(baseURL);
+dataReq.then(function (res) {
+  console.log(res.results);
+  res.results.forEach(function (x) {
+    console.log(x.picture.thumbnail);
+    var htmldata = user(x);
+    inner.append(htmldata);
+  });
+});
+//
+
+var user = function user(x) {
+  return '\n  <section class="user">\n  <div class="picbox"> <img src = "' + x.picture.large + ' "/></div>\n  <div class="textbox" >\n\n  <h3> <span> ' + x.name.first + ' ' + x.name.last + '</span> </h3> <h4>' + x.email + '</h4> <h5>' + x.location.street + '</h5> <h5>' + x.cell + '</h5> <h5 class="ssn">' + x.id.value + '</h5></div>\n  </section>\n  ';
+};
+//
+// var userImage = function (user {})
+//
+//
+// var inner = $('.inner');
+// console.log(url);
 
 },{"jquery":1}]},{},[2])
 
--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
 import $ from 'jquery';
+//console.log($);
 //
 // $.ajax({
 //   url: 'https://randomuser.me/api/',
@@ -8,10 +9,38 @@ import $ from 'jquery';
 //   }
 // });
 
-
-var baseURL = 'https://randomuser.me/api/?results=12&nat=us';
-var dataReq = $.getJSON(url);
-
+ //var url= 'https://randomuser.me/api/';
+// var dataReq = $.getJSON(url).then(function(res){
+//   console.log('res',res);
+//});
 
 var inner = $('.inner');
-console.log(url);
+var baseURL = 'https://randomuser.me/api/?results=12&nat=us';
+ var dataReq = $.getJSON(baseURL);
+ dataReq.then(function(res){
+   console.log(res.results);
+   res.results.forEach(function(x){
+     console.log(x.picture.thumbnail);
+    var htmldata= user(x);
+    inner.append(htmldata);
+   })
+ })
+//
+
+var user = function(x){
+  return`
+  <section class="user">
+  <div class="picbox"> <img src = "${x.picture.large} "/></div>
+  <div class="textbox" >
+
+  <h3> <span> ${x.name.first} ${x.name.last}</span> </h3> <h4>${x.email}</h4> <h5>${x.location.street}</h5> <h5>${x.cell}</h5> <h5 class="ssn">${x.id.value}</h5></div>
+  </section>
+  `;
+
+}
+//
+// var userImage = function (user {})
+//
+//
+// var inner = $('.inner');
+// console.log(url);
